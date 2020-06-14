8b3840b84f588a93c0e1f0933d4faa6e39647339
Federico Abdo <fabdo@correo.um.edu.uy>
2020-06-14 13:29:04 -0300

    cargar datos plan anterior

--- www/custom/custom.js
+++ www/custom/custom.js
@@ -4,13 +4,13 @@ function deviceReady() {
 	try {
 		//Example when Internet connection is needed but not mandatory
 		//Ejemplo de cuando se necesita conexióna a Internet pero no es obligatoria.
-		if (!mui.connectionAvailable()){
+		if (!mui.connectionAvailable()) {
 			if ('plugins' in window && 'toast' in window.plugins)
 				mui.toast('We recommend you connect your device to the Internet');
 			else
 				mui.alert('We recommend you connect your device to the Internet');
 		}
-		
+
 		//Install events, clicks, resize, online/offline, etc. 
 		installEvents();
 
@@ -31,7 +31,7 @@ function deviceReady() {
  * Instala eventos, clicks, resize, online/offline, etc., sobre diferentes elementos HTML.
  */
 function installEvents() {
-	
+
 	mui.util.installEvents([
 		//Mail list click/touch events. See that if the event is not specified, click is assumed.
 		{
@@ -61,18 +61,17 @@ function installEvents() {
 		{
 			id: '#option1',
 			fn: () => {
-				mui.screen.closePanel(function() {
+				mui.screen.closePanel(function () {
 					mui.viewport.showPage("home-page", "DEF");
 				});
 				return false;
 			}
 		},
 		{
-			id: '#option2',
+			id: '.CargarDatosPlanAnterior',
+			ev: 'click',
 			fn: () => {
-				mui.screen.closePanel(function() {
-					mui.viewport.showPage("template-page", "DEF");
-				});
+				mui.viewport.showPage("template-page", "DEF");
 				return false;
 			}
 		},
@@ -80,35 +79,35 @@ function installEvents() {
 		{
 			id: '#tabbar-button1',
 			fn: () => {
-				mui.alert("tab 1","Selected");
+				mui.alert("tab 1", "Selected");
 				return false;
 			}
 		},
 		{
 			id: '#tabbar-button2',
 			fn: () => {
-				mui.alert("tab 2","Selected");
+				mui.alert("tab 2", "Selected");
 				return false;
 			}
 		},
 		{
 			id: '#tabbar-button3',
 			fn: () => {
-				mui.alert("tab 3","Selected");
+				mui.alert("tab 3", "Selected");
 				return false;
 			}
 		},
 		{
 			id: '#tabbar-button4',
 			fn: () => {
-				mui.alert("tab 4","Selected");
+				mui.alert("tab 4", "Selected");
 				return false;
 			}
 		},
 		{
 			id: '#tabbar-button5',
 			fn: () => {
-				mui.alert("tab 5","Selected");
+				mui.alert("tab 5", "Selected");
 				return false;
 			}
 		},
@@ -164,94 +163,103 @@ function installEvents2() {
 
 	//It's a good idea to consider what happens when the device is switched on and off the internet.
 	//Es buena idea considerar que pasa cuando el dispositivo se conecta y desconecta a Internet.
-	document.addEventListener("online", function() {
+	document.addEventListener("online", function () {
 		//somthing
 	}, false);
-	
+
 	//Back button.
-	$(".mui-backarrow").click(function() {
+	$(".mui-backarrow").click(function () {
 		mui.history.back();
 		return false;
 	});
-	
+
 	//Open menu.
-	$(".mui-headmenu").click(function() {
+	$(".mui-headmenu").click(function () {
 		mui.screen.showPanel("menu-panel", "SLIDE_LEFT");	//ATTENTION!!! mui.screen instead of mui.viewport
 		return false;
 	});
 
-	$("#tabbar-button1").click(function() {
-		mui.alert("tab 1","Selected");
+	$("#tabbar-button1").click(function () {
+		mui.alert("tab 1", "Selected");
 		return false;
 	});
-	
-	$("#tabbar-button2").click(function() {
-		mui.alert("tab 1","Selected");
+
+	$("#tabbar-button2").click(function () {
+		mui.alert("tab 1", "Selected");
 		return false;
 	});
-	
-	$("#tabbar-button3").click(function() {
-		mui.alert("tab 3","Selected");
+
+	$("#tabbar-button3").click(function () {
+		mui.alert("tab 3", "Selected");
 		return false;
 	});
-	
-	$("#tabbar-button4").click(function() {
-		mui.alert("tab 4","Selected");
+
+	$("#tabbar-button4").click(function () {
+		mui.alert("tab 4", "Selected");
 		return false;
 	});
-	
-	$("#tabbar-button5").click(function() {
-		mui.alert("tab 5","Selected");
+
+	// $('#CargarDatosPlanAnterior').click(() => {
+	// 	mui.viewport.showPage("template-page", "DEF");
+	// 	return false;
+	// });
+
+	$("#tabbar-button5").click(function () {
+		mui.alert("tab 5", "Selected");
 		return false;
 	});
-	
-	$("#menuoptions").click(function() {
+
+	$("#menuoptions").click(function () {
 		return false;
 	});
-	
+
 	/*******************************************************************************/
 	/*Swipe Test --------------------------------------------------------------------*/
-	/*******************************************************************************/	
+	/*******************************************************************************/
 	//Swipe touch events. Cool for best App user experience!
 	//Evento de desplazamiento tactil. Buenisimo para una óptima experiencia de usuario en App!
-	mui.viewport.on("swiperight", function(currentPageId, originalTarget, event, startX, startY, endX, endY) {
+	mui.viewport.on("swiperight", function (currentPageId, originalTarget, event, startX, startY, endX, endY) {
 		if (!mui.viewport.panelIsOpen()) {
 			mui.history.back();
 		}
 	});
 }
 
-function alumno_upload(){
-	let alumno_upload_html = $('<div class="alumno-upload"><br><button>Nueva sesion</button><br><button>Plan nuevo</button><br><button>Plan anterior</button><br></div>')
+function alumno_upload() {
+	let alumno_upload_html = $('<div class="alumno-upload"><br><button>Nueva sesion</button><br><button>Plan nuevo</button><br><button class="CargarDatosPlanAnterior">Plan anterior</button><br></div>')
 
 	$('li a', ".lista_alumnos").append(alumno_upload_html);
 	$('.alumno-upload').hide();
 }
 
-$(document).ready(()=>{
+$(document).ready(() => {
 	alumno_upload();
 });
 
-function alumno_upload_onclick(event){
-	$(".alumno-upload").not($(".alumno-upload",event)).slideUp()
-	$(".alumno-upload",event).slideToggle();
+function alumno_upload_onclick(event) {
+	$(".alumno-upload").not($(".alumno-upload", event)).slideUp()
+	$(".alumno-upload", event).slideToggle();
 }
 
-function filtro_alumnos(){
+function filtro_alumnos() {
 	var input, filter, ul, li, a, i, txtValue;
 	input = $('.filtro_alumnos');
 	filter = input.val().toUpperCase();
 	ul = $(".lista_alumnos");
 	li = $('li', ".lista_alumnos");
-  
+
 	// Loop through all list items, and hide those who don't match the search query
 	for (i = 0; i < li.length; i++) {
-	  a = $("a", li[i]);
-	  txtValue = a.text();
-	  if (txtValue.toUpperCase().indexOf(filter) > -1) {
-		li[i].style.display = "";
-	  } else {
-		li[i].style.display = "none";
-	  }
+		a = $("a", li[i]);
+		txtValue = a.text();
+		if (txtValue.toUpperCase().indexOf(filter) > -1) {
+			li[i].style.display = "";
+		} else {
+			li[i].style.display = "none";
+		}
 	}
 }
+
+
+
+
