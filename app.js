e88311314dd91f6821398b7515a5a48945eaccaa
Joaquin Martinez <joaco1509@gmail.com>
2020-07-06 22:32:32 -0300

    Elimine la coneccion a la bdd porque no va aca, va en la API rest

--- app.js
+++ app.js
@@ -14,11 +14,6 @@ app.listen(port,  function () {
 	console.log('http://localhost:' + port);
 });
 
-//conectar a bdd
-mongoose.connect('mongodb://localhost/vitalfit', { useUnifiedTopology: true , useNewUrlParser: true })
-    .then(db => console.log('DB connected'))//tira este mensaje si se conecto
-    .catch(err => console.log(err))//tira esto si no conecto
-
 //importing routes
 const indexRoutes = require('./src/routes/index');//le aviso que aca estan las rutas
 //routes
